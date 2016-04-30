ba1434dfff4ae6afd8e7b8a522925af38743eeba
jcacosta <juank2acosta@gmail.com>
2016-04-30 11:25:45 -0500

    Cambios en recyclerview

--- app/src/main/java/com/fsw_fime/myfirstapp/Clima.java
+++ app/src/main/java/com/fsw_fime/myfirstapp/Clima.java
@@ -6,15 +6,11 @@ package com.fsw_fime.myfirstapp;
  * juan.acosta@bbxmstudios.com
  */
 public class Clima {
-    String temp, humedad,ciudad,prob,imagen,dia;
+    String temp, ciudad;
 
-    public Clima(String temp, String humedad, String ciudad, String prob, String imagen, String dia) {
+    public Clima(String temp, String ciudad) {
         this.temp = temp;
-        this.humedad = humedad;
         this.ciudad = ciudad;
-        this.prob = prob;
-        this.imagen = imagen;
-        this.dia = dia;
     }
 
     public String getTemp() {
@@ -25,43 +21,11 @@ public class Clima {
         this.temp = temp;
     }
 
-    public String getHumedad() {
-        return humedad;
-    }
-
-    public void setHumedad(String humedad) {
-        this.humedad = humedad;
-    }
-
     public String getCiudad() {
         return ciudad;
     }
 
     public void setCiudad(String ciudad) {
         this.ciudad = ciudad;
-    }
-
-    public String getProb() {
-        return prob;
-    }
-
-    public void setProb(String prob) {
-        this.prob = prob;
-    }
-
-    public String getImagen() {
-        return imagen;
-    }
-
-    public void setImagen(String imagen) {
-        this.imagen = imagen;
-    }
-
-    public String getDia() {
-        return dia;
-    }
-
-    public void setDia(String dia) {
-        this.dia = dia;
     }
 }
--- app/src/main/java/com/fsw_fime/myfirstapp/WeatherAdapter.java
+++ app/src/main/java/com/fsw_fime/myfirstapp/WeatherAdapter.java
@@ -1,9 +1,59 @@
 package com.fsw_fime.myfirstapp;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
 /**
  * Created by Juan C. Acosta on 4/30/2016.
  * bbxmstudios
  * juan.acosta@bbxmstudios.com
  */
-public class WeatherAdapter {
+public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.MyViewHolder>{
+
+    private List<Clima> climaList;
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public TextView temp,ciudad;
+
+        public MyViewHolder(View v){
+            super(v);
+            temp = (TextView) v.findViewById(R.id.txt_temp);
+            ciudad = (TextView) v.findViewById(R.id.txt_city);
+        }
+
+
+    }
+
+    public WeatherAdapter(List<Clima> climaList){
+        this.climaList = climaList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.clima_layout,parent,false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Clima clima = climaList.get(position);
+        holder.ciudad.setText(clima.getCiudad());
+        holder.temp.setText(clima.setTemp());
+    }
+
+    @Override
+    public int getItemCount() {
+        return climaList.size();
+    }
+
+
+
+
 }
